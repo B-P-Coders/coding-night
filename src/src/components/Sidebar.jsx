e8a5f1e42f107b72d7400fa4506ec4b6b9263f61
n00sek <74304320+n00sek@users.noreply.github.com>
2021-11-19 22:09:10 +0100

    a

--- src/src/components/Sidebar.jsx
+++ src/src/components/Sidebar.jsx
@@ -6,14 +6,14 @@ export default function Sidebar () {
   return (
     <div className={styles.main}>
       <div className={styles.logo}>
-        fsjafn
+        <img src="" alt="" />
       </div>
-      <link rel="stylesheet" href="/themes/default.css" />
+
       <ul className={styles.list}>
-        <li><Link to="/"><i class="fas fa-calculator" alt="Home" /></Link></li>
-        <li><Link to="/calculator"><img src="" alt="Calculator" /></Link></li>
-        <li><Link to="/notes"><img src="" alt="Notes" /></Link></li>
-        <li><Link to="/timetable"><img src="" alt="Timetable" /></Link></li>
+        <li><Link to="/"><img src="../images/graduation-cap-solid.svg" alt="Home" /></Link></li>
+        <li><Link to="/calculator"><img src="../images/calculator-solid.svg" alt="Calculator" /></Link></li>
+        <li><Link to="/notes"><img src="../images/sticky-note-solid.svg" alt="Notes" /></Link></li>
+        <li><Link to="/timetable"><img src="../images/clipboard-list-solid.svg" alt="Timetable" /></Link></li>
       </ul>
 
       <div>
