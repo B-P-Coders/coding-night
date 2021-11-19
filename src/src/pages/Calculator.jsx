32179b96b48083822e898206f724f5b3129fb5b0
bart016 <bart.turczynski@gmail.com>
2021-11-20 00:29:50 +0100

    k

--- src/src/pages/Calculator.jsx
+++ src/src/pages/Calculator.jsx
@@ -1,38 +1,42 @@
 import React from "react";
 import * as styles from "../styles/calculator.module.css"
+import Iframe from 'react-iframe'
+
 export default function Calculator(){
-var currentNum = '';
-var total = 0;
-var operation = '';
+const createDigits = () =>{
+  const digits = [];
+
+    for(let i=1;i<10;i++){
+      digits.push(
+        <button key={i}>{i}></button>
+      )
+    }
+    return digits;
+}
 
 return(
+  <div className="App">
+<div className="calculator">
+  <div className="display">
+<span>(0)</span>0
+  </div>
+  <div className="operators">
+<button>/</button>
+<button>*</button>
+<button>+</button>
+<button>-</button>
 
-<div id="calculator">
+<button>DEL</button>
+  </div>
+<div className="digits">
+  {createDigits()}
+<button>0</button>
+<button>-</button>
+<button>=</button>
+</div>
 
-    <div id="screen">
-      <p>0</p>
-    </div>
-    <div id="buttonPad">
-      <button id="ca">CA</button>
-      <button id="7">7</button>
-      <button id="4">4</button>
-      <button id="1">1</button>
-      <button id="0">0</button>
-      <button id="ce">CE</button>
-      <button id="8">8</button>
-      <button id="5">5</button>
-      <button id="2">2</button>
-      <button id="decimal">.</button>
-      <button id="percent">%</button>
-      <button id="9">9</button>
-      <button id="6">6</button>
-      <button id="3">3</button>
-      <button id="equals">=</button>
-      <button id="divide">/</button>
-      <button id="times">x</button>
-      <button id="minus">-</button>
-      <button id="plus">+</button>
-      </div>
-      </div>
+
+</div>
+</div>
 )
 }
