1534d4ee8787fd4c2959896ed9231dd73df114b0
n00sek <74304320+n00sek@users.noreply.github.com>
2021-11-19 23:20:14 +0100

    a

--- src/src/components/Card.jsx
+++ src/src/components/Card.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import styles from '../styles/card.module.css'
+import picture from '../images/doc.png'
+
 
 export default function Card(props) {
+  console.log(props.pic)
   return (
     <div className={styles.main}>
-      <div className={styles.pic}>{props.pic}</div>
+      <link rel="stylesheet" href="/themes/default.css" />
+      <div className={styles.pic}><img src={picture} alt="essa" /></div>
       <div className={styles.title}>{props.title}</div>
     </div>
   )
--- src/src/pages/Index.jsx
+++ src/src/pages/Index.jsx
@@ -3,16 +3,17 @@ import Layout from '../components/Layout'
 import * as styles from '../styles/index.module.css'
 import Header from '../components/Header'
 import Card  from "../components/Card";
-import Pic from "../images/doc.png";
-
 
 export default function Index () {
   return (
     <Layout>
       <div className={styles.main}>
       <Header value="Welcome to school4u! A place where you can manage your time efficiently!"/>
-      <Card pic={Pic} value='This app contains numerous tools:' />
-        <p>This app contains numerous tools:</p>
+      <div className={styles.cardsContainer}>
+        <Card pic={'../images/doc.png'} title="Check out our's app options" />
+      </div>
+
+        <p>Check out our's app options</p>
         <ul>
             <li>Time table</li>
             <li>Calendar</li>
