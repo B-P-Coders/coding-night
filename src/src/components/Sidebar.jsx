1f40b622ed1532c5404a48243b0c7bc29a4a2a86
OreoPL-studio <81591050+OreoPL-studio@users.noreply.github.com>
2021-11-19 22:21:30 +0100

    FIxed icons

--- src/src/components/Sidebar.jsx
+++ src/src/components/Sidebar.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styles from '../styles/sidebar.module.css';
 import { Link } from 'react-router-dom';
+import Graduation from '../images/graduation-cap-solid.svg'
+import Calculator_solid from '../images/calculator-solid.svg'
+import Sticky from '../images/sticky-note-solid.svg'
+import Clipboard from '../images/clipboard-list-solid.svg'
 
 export default function Sidebar () {
   return (
@@ -10,10 +14,10 @@ export default function Sidebar () {
       </div>
 
       <ul className={styles.list}>
-        <li><Link to="/"><img src="../images/graduation-cap-solid.svg" alt="Home" /></Link></li>
-        <li><Link to="/calculator"><img src="../images/calculator-solid.svg" alt="Calculator" /></Link></li>
-        <li><Link to="/notes"><img src="../images/sticky-note-solid.svg" alt="Notes" /></Link></li>
-        <li><Link to="/timetable"><img src="../images/clipboard-list-solid.svg" alt="Timetable" /></Link></li>
+        <li><Link to="/"><img src={Graduation} alt="Home" /></Link></li>
+        <li><Link to="/calculator"><img src={Calculator-solid} alt="Calculator" /></Link></li>
+        <li><Link to="/notes"><img src={Sticky} alt="Notes" /></Link></li>
+        <li><Link to="/timetable"><img src={Clipboard} alt="Timetable" /></Link></li>
       </ul>
 
       <div>
