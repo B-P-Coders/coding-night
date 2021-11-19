39a5eb358688d8ed8e1bcd5ddcc2cc20ff3bdb96
OreoPL-studio <81591050+OreoPL-studio@users.noreply.github.com>
2021-11-19 22:28:42 +0100

    Fixed icons #2

--- src/src/components/Sidebar.jsx
+++ src/src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../styles/sidebar.module.css';
 import { Link } from 'react-router-dom';
 import Graduation from '../images/graduation-cap-solid.svg'
-import Calculator_solid from '../images/calculator-solid.svg'
+import CalculatorSolid from '../images/calculator-solid.svg'
 import Sticky from '../images/sticky-note-solid.svg'
 import Clipboard from '../images/clipboard-list-solid.svg'
 
@@ -15,7 +15,7 @@ export default function Sidebar () {
 
       <ul className={styles.list}>
         <li><Link to="/"><img src={Graduation} alt="Home" /></Link></li>
-        <li><Link to="/calculator"><img src={Calculator-solid} alt="Calculator" /></Link></li>
+        <li><Link to="/calculator"><img src={CalculatorSolid} alt="Calculator" /></Link></li>
         <li><Link to="/notes"><img src={Sticky} alt="Notes" /></Link></li>
         <li><Link to="/timetable"><img src={Clipboard} alt="Timetable" /></Link></li>
       </ul>
