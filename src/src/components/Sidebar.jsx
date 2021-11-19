870a4d71d79897d9381bc8ea37f8190e866c071c
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-11-20 00:09:54 +0100

    Icons & title

--- src/src/components/Sidebar.jsx
+++ src/src/components/Sidebar.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import styles from '../styles/sidebar.module.css';
 import { Link } from 'react-router-dom';
-import Graduation from '../images/graduation-cap-solid.svg'
-import CalculatorSolid from '../images/calculator-solid.svg'
-import Sticky from '../images/sticky-note-solid.svg'
-import Clipboard from '../images/clipboard-list-solid.svg'
 
 export default function Sidebar () {
   return (
@@ -14,10 +10,10 @@ export default function Sidebar () {
       </div>
 
       <ul className={styles.list}>
-        <li><Link to="/"><img src={Graduation} alt="Home" /></Link></li>
-        <li><Link to="/calculator"><img src={CalculatorSolid} alt="Calculator" /></Link></li>
-        <li><Link to="/notes"><img src={Sticky} alt="Notes" /></Link></li>
-        <li><Link to="/timetable"><img src={Clipboard} alt="Timetable" /></Link></li>
+        <li><Link to="/"><i class="fas fa-graduation-cap"></i></Link></li>
+        <li><Link to="/calculator"><i class="fas fa-calculator"></i></Link></li>
+        <li><Link to="/notes"><i class="fas fa-sticky-note"></i></Link></li>
+        <li><Link to="/timetable"><i class="fas fa-clipboard-list"></i></Link></li>
       </ul>
 
       <div>
