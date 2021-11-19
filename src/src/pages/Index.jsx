5a4868f8ae8d6b79d9a4ad12b06516d34c93a04d
OreoPL-studio <81591050+OreoPL-studio@users.noreply.github.com>
2021-11-20 00:41:19 +0100

    Grammar fix

--- src/src/pages/Index.jsx
+++ src/src/pages/Index.jsx
@@ -12,7 +12,7 @@ export default function Index () {
       <div className={styles.main}>
       <Header value="Welcome to school4u! A place where you can manage your time efficiently!"/>
       <div className={styles.cardsContainer}>
-        <Card pic={'../images/doc.png'} title="Check out our's app options" desc="We've made some amazing tools just for you! It's all to help you get better in any subject!" values={list}/>
+        <Card pic={'../images/doc.png'} title="Check out our app's options" desc="We've made some amazing tools just for you! It's all to help you get better in any subject!" values={list}/>
       </div>
       </div>
 
