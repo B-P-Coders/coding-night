609d2664b5a6b3a9bd36ca081159cfbe5ed4a9d7
n00sek <74304320+n00sek@users.noreply.github.com>
2021-11-19 21:16:52 +0100

    a

--- src/src/components/Sidebar.jsx
+++ src/src/components/Sidebar.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import styles from '../styles/sidebar.module.css';
-import
+import { Link } from 'react-router-dom';
 
 export default function Sidebar () {
-  console.log(styles)
   return (
     <div className={styles.main}>
-
+      <div className={styles.logo}>
+        fsjafn
+      </div>
+      <ul className={styles.list}>
+        <li><Link to="/"> <img src="" alt="Home" /></Link></li>
+        <li><Link to="/calculator"><img src="" alt="Calculator" /></Link></li>
+        <li><Link to="/notes"><img src="" alt="Notes" /></Link></li>
+        <li><Link to="/timetable"><img src="" alt="Timetable" /></Link></li>
+      </ul>
     </div>
   )
 }
