a8986f5ced4d5d2463608bc484de66d05565a9eb
bart016 <bart.turczynski@gmail.com>
2021-11-20 04:24:59 +0100

    formula

--- src/src/pages/Formulas.jsx
+++ src/src/pages/Formulas.jsx
@@ -5,7 +5,8 @@ export default function Formulas() {
 return (
   <Layout>
   <div className={styles.main}>
-<p>Math</p>
+    <div className = {styles.math}>
+<p className={styles.paragraph}>Math</p>
 <div className={styles.formula}> n! = 1 · 2 · 3 · ... · n</div>
 <div className={styles.formula}>sin(α + β) = sinαcosβ + cosαsinβ</div>
 <div className={styles.formula}>  Pn = n!</div>
@@ -14,7 +15,8 @@ return (
 <div className={styles.formula}> V = Pp · H</div>
 <div className={styles.formula}>sin(270° + α) = -cosα </div>
 <div className={styles.formula}>a3 - b3 = (a - b)(a2 + ab + b2)</div>
-<p>Physics</p>
+    </div><div className={styles.physics}>
+<p className={styles.paragraph}>Physics</p>
 <div className={styles.formula}>Fg = m • g</div>
 <div className={styles.formula}>ρ = m/V</div>
 <div className={styles.formula}>p = Fn/S</div>
@@ -24,13 +26,14 @@ return (
 <div className={styles.formula}>a = ΔV/Δt</div>
 <div className={styles.formula}>V = a • t</div>
 <div className={styles.formula}>a = F/m</div>
-
-<p>Chemistry</p>
+</div><div className={styles.chemistry}>
+<p className={styles.paragraph}>Chemistry</p>
 <div className={styles.formula}>cm=n/Vr</div>
 <div className={styles.formula}>cm=(cp⋅dr)(100%⋅M)</div>
-<div className={styles.formula}></div>
-<div className={styles.formula}></div>
-<div className={styles.formula}></div>
+<div className={styles.formula}>d=m/v</div>
+<div className={styles.formula}>pH + pOH = 14</div>
+<div className={styles.formula}>pV=nRT</div>
+</div>
 
   </div>
   </Layout>
