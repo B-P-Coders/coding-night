fca6dfb352843e13c769c5f32b14f3244e281f87
bart016 <bart.turczynski@gmail.com>
2021-11-20 03:31:06 +0100

    start formulas

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -5,6 +5,7 @@ import Notes from './pages/Notes';
 import Math from './pages/Math'
 import Timetable from './pages/Timetable';
 import Calculator from './pages/Calculator';
+import Formulas from './pages/Formulas';
 import Charts from './pages/FalconFunction'
 
 export default function App()
@@ -17,7 +18,7 @@ export default function App()
             <Route path='/timetable' element={<Timetable/>}/>
             <Route path='/notes' element={<Notes />}/>
             <Route exact path='/' element={<Index/>} />
-            {/* <Route path='/formulas' element={<Formulas />}/> */}
+           <Route path='/formulas' element={<Formulas />}/>
             <Route path='/charts' element={<Charts/>}/>
             <Route path='/calculator' element={<Calculator />}/>
           </Routes>
---
+++ src/src/pages/Formulas.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as styles from '../styles/formulas.module.css'
+import Layout from '../components/Layout'
+export default function Formulas() {
+return (
+  <Layout>
+  <div className={styles.main}>
+
+aaaaaaaaaaa
+
+
+  </div>
+  </Layout>
+
+)
+}
