c50221800cf9ba1fcbb5af9c5d7c412e916ba418
n00sek <n00sek@onet.pl>
2022-03-16 19:05:29 +0100

    prosze to ostylowac

---
+++ src/src/components/Canvas.jsx
@@ -0,0 +1,115 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import * as styles from '../styles/canvas.module.css';
+
+const colors = [
+  "red",
+  "green",
+  "yellow",
+  "black",
+  "blue",
+  "pink",
+  "purple",
+  "whitesmoke"
+]
+
+const sizes = [
+  1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+]
+
+function Canvas() {
+  const canvasRef = useRef(null);
+  const ctx = useRef(null);
+
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
+  const [selectedSize, setSelectedSize] = useState(sizes[0]);
+  const [mouseDown, setMouseDown] = useState(false);
+  const [lastPosition, setPosition] = useState({
+    x: 0,
+    y: 0
+  });
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      ctx.current = canvasRef.current.getContext('2d');
+      console.log(canvasRef.current.getBoundingClientRect())
+    }
+  }, []);
+
+  const draw = useCallback((x, y) => {
+    if (mouseDown) {
+      ctx.current.beginPath();
+      ctx.current.strokeStyle = selectedColor;
+      ctx.current.lineWidth = selectedSize;
+      ctx.current.lineJoin = 'round';
+      ctx.current.moveTo(lastPosition.x, lastPosition.y);
+      ctx.current.lineTo(x, y);
+      ctx.current.closePath();
+      ctx.current.stroke();
+
+      setPosition({
+        x,
+        y
+      })
+    }
+  }, [lastPosition, mouseDown, selectedColor, setPosition])
+
+  const download = async () => {
+    const image = canvasRef.current.toDataURL('image/png');
+    const blob = await (await fetch(image)).blob();
+    const blobURL = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = blobURL;
+    link.download = "image.png";
+    link.click();
+  }
+
+  const clear = () => {
+    ctx.current.clearRect(0, 0, ctx.current.canvas.width, ctx.current.canvas.height)
+  }
+
+  const onMouseDown = (e) => {
+    setPosition({
+      x: e.clientX - canvasRef.current.getBoundingClientRect().left,
+      y: e.clientY - canvasRef.current.getBoundingClientRect().top
+    })
+    setMouseDown(true)
+  }
+
+  const onMouseUp = (e) => {
+    setMouseDown(false)
+  }
+
+  const onMouseMove = (e) => {
+    draw(e.clientX - canvasRef.current.getBoundingClientRect().left, e.clientY - canvasRef.current.getBoundingClientRect().top)
+  }
+
+  return (
+    <>
+      <canvas
+        width={1000}
+        height={700}
+        ref={canvasRef}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onMouseLeave={onMouseUp}
+        onMouseMove={onMouseMove}
+      />
+      <br />
+      <input type="color" value={selectedColor} onChange={(e) => setSelectedColor(e.target.value)}/>
+      <select
+        value={selectedSize}
+        onChange={(e) => setSelectedSize(e.target.value)}
+      >
+        {
+          sizes.map(
+            size => <option key={size} value={size}>{size}</option>
+          )
+        }
+      </select>
+      <button onClick={clear}>Clear</button>
+      <button onClick={download}>Download</button>
+    </>
+  );
+}
+
+export default Canvas;
--- src/src/pages/Draw.jsx
+++ src/src/pages/Draw.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Layout from '../components/Layout'
+import Canvas from '../components/Canvas'
 
 export default function Draw() {
   return (
     <Layout>
       <Header value="Drawing app"/>
       <main>
-        <canvas>
-
-        </canvas>
+        <Canvas/>
       </main>
     </Layout>
   )
