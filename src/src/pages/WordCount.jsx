a99a5115725d0e4f2dd5cb802260f13830ffa4d8
OreoPL-studio <81591050+OreoPL-studio@users.noreply.github.com>
2022-03-17 08:41:43 +0100

    koniec word counta

--- src/src/pages/WordCount.jsx
+++ src/src/pages/WordCount.jsx
@@ -2,17 +2,29 @@ import React, {useRef, useState} from 'react'
 import * as styles from "../styles/word.module.css"
 
 const WordCount = () => {
-    let countTarget = useRef(null).current;
-    
+    var countTarget = useRef(null).current;
+    var wordCount = useRef(null).current;
+    var characterCount = useRef(null).current;
+    var count = function() {
+        var characters = countTarget.value;
+        var characterLength = characters.length;
+        var words = characters.split(/[\n\r\s]+/g).filter(function(word){
+            return word.length;
+        });
+        wordCount.innerHTML = words.length;
+        characterCount.innerHTML = characterLength;
+    };
+    count();
+    window
     
     return (
         <div className="uiInputContainer">
             <h2>Word Count</h2>
             <label className="uiFormInput-container">
-                <textarea className="uiFormInput" id="wordCountInput" ref={countTarget}></textarea>
+                <textarea className="uiFormInput" id="wordCountInput" ref={countTarget} onInput={(e) => e.target.matches("#wordCountInput")}></textarea>
                 <span className="formInputLabel">Message</span>
             </label>
-            <p aria-live="polite"><strong><span id="wordCount">0</span> words</strong> | <strong><span id="characterCount">0</span> characters</strong></p>
+            <p aria-live="polite"><strong><span id="wordCount" ref={wordCount}>0</span> words</strong> | <strong><span id="characterCount" ref={characterCount}>0</span> characters</strong></p>
         </div>
     )
 }
